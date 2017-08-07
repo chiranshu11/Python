b8821882d1e50cc4784e80733f3d41866771b2e9
chiranshu11 <ca1897@gmail.com>
2017-08-07 22:57:43 +0530

    Python Programs

---
+++ Mid Libs Generator1/Weekend.py
@@ -0,0 +1,15 @@
+# Defining a procedure weekend which takes a string as its input, and
+# returns the boolean True if it's 'Saturday' or 'Sunday' and False otherwise.
+
+def weekend(day):
+    return day[0]=='S'
+
+
+print weekend('Monday')
+#>>> False
+
+print weekend('Saturday')
+#>>> True
+
+print weekend('July')
+#>>> False
---
+++ lists/Working_with_nested_lists.py
@@ -0,0 +1,17 @@
+# Given the variable countries defined as:
+
+#             Name    Capital  Populations (millions)
+countries = [['China','Beijing',1350],
+             ['India','Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington',307]]
+
+# code to print out the capital of India
+# by accessing the list
+
+
+#According to last example, created list variable but that will take memory
+#so here is the simpler version
+
+print(countries[1][1])
+print(countries[1][2])
